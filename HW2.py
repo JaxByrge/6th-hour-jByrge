48dea67b9a857c268b2fe8e0b51c1e4853034803
Jax Byrge <jb128412@student.wpsok.org>
2024-08-23 13:33:17 -0500

    Added HW2

---
+++ HW2.py
@@ -0,0 +1,11 @@
+#Name: Jax Byrge
+#Hour: 6th Hour
+#Assignment: HW2
+print("Hello World")
+
+name = input("What is your name? ")
+print("Hello", name)
+
+x = int(input("What number would you like to add? "))
+y = int(input("What is the second number you would like to add? "))
+print("Your number is", x+y)
