15f954120c651e8fd21f321407a4d89c1e3d1eef
Jax Byrge <jb128412@student.wpsok.org>
2024-08-28 13:56:31 -0500

    Added HW2

---
+++ HW3.py
@@ -0,0 +1,19 @@
+#Name: Jax Byrge
+#Class: 6th
+#Assignment: HW3
+
+print ("Hello World")
+
+list = ["A","B","C","D","F"]
+list.append(input("What is the 6th letter in the alphebet "))
+print(list)
+print(list[4])
+
+letters = [1, 2, 3, 4,]
+print(letters)
+letters.insert(1,int(input("Pick a number ")))
+print(letters)
+letters.sort()
+print(letters)
+sum = (letters[0] + letters[1] + letters[2] + letters[3])
+print(sum)
