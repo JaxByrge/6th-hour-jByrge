8495c23636aa83e2a814301c67a5087a767282d1
Jax Byrge <jb128412@student.wpsok.org>
2024-09-24 14:06:41 -0500

    Added scenario 2

---
+++ Scenario2.py
@@ -0,0 +1,107 @@
+#Name: Jax
+#Class: 6th Hour
+#Assignment: Scenario 1
+
+#Scenario 2:
+#The programmer in charge of the player character party stats is
+#having some issues with their code. Despite rigorous testing,
+#they are inexperienced and can't seem to figure out why it damage
+#testing isn't working. Your team lead has asked you to help by fixing
+#the party dictionary, insert an enemy into the code below, and
+#testing to see if a player character can damage the with a printed
+#test that shows the enemy health has changed.
+
+import random
+
+partyDictionary = {
+    "LaeZel" : {
+        "Race" : "Githyanki",
+        "Class" : "Fighter",
+        "Background" : "Soldier",
+        "Health" : 12,
+        "AC" : 17,
+        "Damage" : 10,
+    },
+    "Shadowheart" : {
+        "Race" : "Half-Elf",
+        "Class" : "Cleric",
+        "Background" : "Acolyte",
+        "Health" : 10,
+        "AC" : 14,
+        "Damage" : 5,
+    },
+    "Gale" : {
+        "Race" : "Human",
+        "Class" : "Wizard",
+        "Background" : "Sage",
+        "Health" : 8,
+        "AC" : 14,
+        "Damage" : 17,
+    },
+    "Astarion" : {
+        "Race" : "High Elf",
+        "Class" : "Rogue",
+        "Background" : "Charlatan",
+        "Health" : 10,
+        "AC" : 14,
+        "Damage" : 12,
+    }
+}
+
+#Enemy Dictionary goes here
+
+enemyDict = {
+    "Zombo" : {
+        "Damage" : 15,
+        "Health" : 15,
+        "AC" : 6,
+    },
+    "Bogo": {
+        "Damage" : 5,
+        "Health" : 12,
+        "AC" : 17,
+    },
+    "Trogo": {
+        "Damage" : 12,
+        "Health" :  16,
+        "AC" : 15,
+    },
+    "Thrag": {
+        "Damage" : 22,
+        "Health" : 18,
+        "AC" : 13,
+    },
+    "Drackon": {
+        "Damage" : 30,
+        "Health" : 35,
+        "AC" : 18,
+    }
+}
+
+#Test the damage here by subtracting a party member's damage from the enemy's health.
+#x = (enemyDict["Zombo"]["Health"]) - (partyDictionary["LaeZel"]["Damage"])
+#print(x)
+
+randomEncounters = ("Zombo", "Bogo", "Trogo", "Thrag", "Drackon")
+encounter = random.choice(randomEncounters)
+print("You have enountered a wild", encounter)
+print("Which party member do you want to send forward")
+partyMember = input("[LaeZel] [Shadowheart] [Gale] [Astarion] : ")
+print(partyMember)
+playerHealth = partyDictionary[partyMember]["Health"]
+playerDamage = partyDictionary[partyMember]["Health"]
+playerAc = partyDictionary[partyMember]["AC"]
+if partyMember != "LaeZel":
+    if partyMember != "Shadowheart":
+        if partyMember != "Gale":
+            if partyMember != "Astarion":
+                playerHealth = 10
+                playerDamage = 10
+                playerAc = 10
+enemyHealth = enemyDict[encounter]["Health"]
+enemyDamage = enemyDict[encounter]["Damage"]
+enemyAc = enemyDict[encounter]["AC"]
+print("[",partyMember,"]   [",encounter,"]")
+print("[HP]",playerHealth,"    [HP]",enemyHealth)
+print("[STR]",playerDamage,"   [STR]",enemyDamage)
+print("[DEF]",playerAc,"  [DEF]",enemyAc)
