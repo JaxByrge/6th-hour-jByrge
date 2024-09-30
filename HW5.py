b7a3cc8e9c43e383c067c45e1f4b5b3953ada278
Jax Byrge <jb128412@student.wpsok.org>
2024-09-30 13:24:54 -0500

    HW5

--- HW5.py
+++ HW5.py
@@ -6,12 +6,12 @@
 print("Hello World")
 
 #2. Create a list that contains 3 integers
-numList = [5,10,15]
+numList = [50,1,9]
 
 #3. Create an if statement that prints out whichever of the three numbers is the highest
 if numList[0] > numList[1] and numList[0] > numList[2]:
-    print("The first intiger is the greatest number")
+    print("The first integer is the greatest number")
 if numList[1] > numList[2] and numList[1] > numList[0]:
-    print("The second intiger is the greatest number")
+    print("The second integer is the greatest number")
 if numList[2] > numList[1] and numList[2] > numList[0]:
-    print("The third intiger is the greatest number")
+    print("The third integer is the greatest number")
