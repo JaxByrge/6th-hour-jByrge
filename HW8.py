71e9ce3f50dcfbcc5fdfd97853ea8a7f2cd25dd8
Jax Byrge <jb128412@student.wpsok.org>
2024-10-22 13:42:40 -0500

    Added HW8

---
+++ HW8.py
@@ -0,0 +1,45 @@
+#Name:Jax Byrge
+#Class: 6th Hour
+#Assignment: HW8
+
+#1. Print Hello World!
+print("hello world")
+#2. Take the variables below and change the name of the variables to match the type of variable they are: string, integer, boolean, or list.
+
+#For example, the first one, Var1 = 4, should be:
+#integerVar1 = 4 or intVar1 = 4
+
+#DO NOT CHANGE THE VALUE OF THE VARIABLE.
+
+int1 = 4
+string1 = "Box"
+boolean1 = True
+string2 = "Nineteen"
+string3 = "Rope"
+int2 = 19
+string4 = "19"
+list1 = ["Window", "Apple", "Penguin"]
+int3 = 2
+boolean2 = True
+boolean3 = False
+string5 = "True"
+string6 = "False"
+list2 = [1, 0]
+list3 = [1]
+list4 = [0]
+int4 = 5
+string7 = "The"
+string8 = "Game"
+int5 = 3
+int6 = 2
+int7 = 1
+list5 = ["Liftoff", True]
+string9 = string5
+string10 = string6
+list6 = list2
+list7 = list3
+list8 = list4
+int8 = 6
+
+#3. Take all the variables you labeled as "integer", add them together, and print the result.
+print(int1+int2+int3+int4+int5+int6+int7+int8)
