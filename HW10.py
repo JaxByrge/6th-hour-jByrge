659eee71a1098c88cac9933893ed2421358fbbb2
Jax Byrge <jb128412@student.wpsok.org>
2024-10-29 13:31:38 -0500

    Added HW10

---
+++ HW10.py
@@ -0,0 +1,23 @@
+#Name: Jax
+#Class: 6th Hour
+#Assignment: HW10
+
+#1. Create a while loop with variable i that counts down from 5 to 0
+#and then prints "Hello World!" at the end.
+i = 6
+while i > 0:
+    i -= 1
+    print(i)
+print("Hello world")
+#2. Create a while loop that prints only even numbers
+#between 1 and 30.
+j = 2
+while j < 31:
+    print(j)
+    j += 2
+#3. Create a while loop that repeats until the user
+#inputs the number 0.
+h = 1
+while h != 0:
+    h = int(input("Input:"))
+    print(h)
