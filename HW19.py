9add0902c9ceb10632c413fc9ecbc3838997fd84
Jax Byrge <jb128412@student.wpsok.org>
2025-02-03 13:28:56 -0600

    Added Hw19

---
+++ HW19.py
@@ -0,0 +1,13 @@
+#Name:
+#Class: 6th Hour
+#Assignment: HW19
+
+#1. Import all the functions from the HW15 file.
+import HW15
+#2. Call the functions here and run HW19.
+helloWorld()
+average()
+animal("Cheeta","Leopard","Lion","Orangutan","Ape")
+loop(int(input("How many loops do you want ")))
+exit(0)
+#3. Delete all calls from HW15 and run HW19 again.
