9173e5a6c4fa9f24d3efebc633d23a6820378c83
Jax Byrge <jb128412@student.wpsok.org>
2025-04-02 13:38:13 -0500

    Added HW25

---
+++ playgroundf.py
@@ -0,0 +1,16 @@
+#Name:Jax Byrge
+#Hour:6
+#Asignemnt:playground
+
+class stats:
+    def __init__(self,fort,str,agility,int,wil,char):
+        self.fort = fort
+        self.str = str
+        self.agility = agility
+        self.int = int
+        self.wil = wil
+        self.char = char
+        self.hp = fort * 2
+        self.maxHp = fort * 2
+
+
